0069d9e1ec4bab410c4d17f8cee7b78ab1727a24
Maciej Kurzawa <maciej.kurzawa@gmail.com>
2012-10-19 14:36:04 +0200

    init commit for mklink module

---
+++ mklink/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+
+from _mklink import mklink
+
+__all__ = ['mklink', ]
---
+++ mklink/_mklink.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+'''
+Created on 19-10-2012
+
+@author: mkurzawa
+'''
+
+class mklink():
+    pass
